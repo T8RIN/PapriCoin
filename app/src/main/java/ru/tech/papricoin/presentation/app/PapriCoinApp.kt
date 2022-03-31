159d15e94ee6c73360b8940bfd1c3d75be09e0d1
T8RIN <oolbp@bk.ru>
2022-03-31 18:39:14 +0300

    init

--- app/src/main/java/ru/tech/papricoin/presentation/app/PapriCoinApp.kt
+++ app/src/main/java/ru/tech/papricoin/presentation/app/PapriCoinApp.kt
@@ -1,14 +1,23 @@
 package ru.tech.papricoin.presentation.app
 
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import ru.tech.papricoin.presentation.ui.theme.PapriCoinTheme
 import ru.tech.papricoin.presentation.utils.WindowSize
 
+@ExperimentalMaterial3Api
 @Composable
 fun PapriCoinApp(windowSize: WindowSize) {
 
     PapriCoinTheme {
+        Scaffold(
 
+        ) {
+
+        }
     }
 
 }
